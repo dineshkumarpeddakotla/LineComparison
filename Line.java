66024815538d059edaf6664c0d344341bd9d3ad0
DINESH KUMAR <dineshkumar.icon@gmail.com>
2021-03-22 23:56:35 +0530

    [dinesh] ADD:Line2 Points MODIFIED:Length UPDATED:Equality check of 2 lines

--- Line.java
+++ Line.java
@@ -13,7 +13,21 @@ System.out.println("Enter the x2 value");
 int x2 =sc.nextInt();
 System.out.println("Enter the y2 value");
 int y2 =sc.nextInt();
-double length =Math.floor(Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)*1.0));
-System.out.println("Length of line = " +length);
+System.out.println("Enter the x3 value");
+int x3 =sc.nextInt();
+System.out.println("Enter the y3 value");
+int y3 =sc.nextInt();
+System.out.println("Enter the x4 value");
+int x4 =sc.nextInt();
+System.out.println("Enter the y4 value");
+int y4 =sc.nextInt();
+
+double length1 =Math.floor(Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
+double length2 =Math.floor(Math.sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2)));
+System.out.println("Length of line one = " +length1);
+System.out.println("Length of line two = " +length2);
+String s1=String.valueOf(length1);
+String s2=String.valueOf(length2);
+System.out.println(s1.equals(s2));
 }
 }
